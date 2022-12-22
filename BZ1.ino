29bfdd9f294848e3969bc03bd572014771b4fb96
Emiliano Hernán Rojas <rojasemilianohernan@gmail.com>
2022-12-22 15:54:43 -0300

    Add BZ1.ino

---
+++ BZ1.ino
@@ -0,0 +1,180 @@
+/*
+ BlitZenz Sensor V0.1
+
+Arduino Sketch for a simple Atmospheric Sensor using an ESP32 NodeMCU module, a DHT11 Temp/Humidiy Sensor and an MQ135 Amospheric Gasses Sensor.
+
+It connects to a HiveMQ Cloud MQTT server then:
+
+-Publishes "OK" to the topic "Start"
+-Subscribes to the topic "in", printing out any string messages it receives.
+-Reads both sensors' outputs and publishes them through the "out" topic
+*/
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include "DHTesp.h" 
+
+
+#define PIN_MQ135 33 // MQ135 Analog Input Pin
+#define DHTPIN 17 // DHT Digital Input Pin
+
+
+//MQ135 mq135_sensor(PIN_MQ135);
+DHTesp dht;
+
+
+// Update these with values suitable for your network.
+
+const char* ssid = "SSID";
+const char* password = "PASS"; //SSID and password of the network to connect to
+const char* mqtt_server = "yourserver.s1.eu.hivemq.cloud";
+const char* user= "USER"; //Change user
+const char* pass= "PASSWORD"; //And Password According to your needs
+const char* clientId = "BZ-1";
+const char* topicStart="FirstTopic/start";
+const char* topicOut="FirstTopic/out";
+const char* topicIn="FirstTopic/in"; //Change FirstTopic with one or more topics
+
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF"; //HiveMQ Generic Certificate, fill with own
+
+TempAndHumidity newValues;
+int co2=1;
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    //clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId,user,pass)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(topicStart, "OK");
+      // ... and resubscribe
+      client.subscribe(topicIn);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(PIN_MQ135, INPUT);
+  Serial.begin(9600);
+  dht.setup(DHTPIN, DHTesp::DHT11);
+  setup_wifi();
+  espClient.setCACert(root_ca);
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
+}
+
+void getMeasurements(){
+  newValues = dht.getTempAndHumidity();
+  co2= analogRead(PIN_MQ135);
+  
+  
+  Serial.print("Temp: ");
+  Serial.print(newValues.temperature);
+  Serial.print("\t Humidity: ");
+  Serial.print(newValues.humidity); 
+  Serial.print("\t CO2: ");
+  Serial.println(co2,DEC);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    getMeasurements();
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "%2.2f,%2.2f,%d", newValues.temperature, newValues.humidity, co2);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(topicOut, msg);
+  }
+}
